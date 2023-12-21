4d790b84560dcad6f8f27dee91c27594e142189d
gianeric08 <gianeric08@gmail.com>
2023-12-20 21:18:04 -0300

    feat: script de controle de personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,16 +14,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update!");
-        Debug.Log(transform.position);
+        float horizontal = SetHorizontal();
+        float vertical = SetVertical();
 
-        // Obtém a posição atual
         Vector2 position = transform.position;
-
-        // Adiciona 0.01 à posição x
-        position.x = position.x + 0.01f;
-
-        // Define a nova posição X
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
+        
+        Debug.Log("Posição do X: " + position.x + " Posição do Y: " +position.y);
+    }
+
+    private float SetHorizontal(){
+        float horizontal = 0.0f;
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -0.1f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 0.1f;
+        }
+
+        return horizontal;
+    }
+
+    private float SetVertical(){        
+        float vertical = 0.0f;
+
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -0.1f;
+        }
+        else if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 0.1f;
+        }
+
+        return vertical;
     }
 }
