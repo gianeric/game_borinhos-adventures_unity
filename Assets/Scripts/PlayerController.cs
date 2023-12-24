f015828ebcffebbe181920e44a8799956d8e3b1c
gianeric08 <gianeric08@gmail.com>
2023-12-24 13:39:37 -0300

    feat: player controller com Move Action Secondary (WASD)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,28 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    public InputAction MoveActionSecondary;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start!");
         MoveAction.Enable();
+        MoveActionSecondary.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        SetMoveAction(MoveAction);
+        SetMoveAction(MoveActionSecondary);
+    }
+
+    private void SetMoveAction(InputAction Move){
+        Vector2 move = Move.ReadValue<Vector2>();
         Vector2 position = (Vector2)transform.position + move * 0.01f;
         transform.position = position;
-        
+
         Debug.Log("Posição do X: " + position.x + " Posição do Y: " +position.y);
     }
 }
