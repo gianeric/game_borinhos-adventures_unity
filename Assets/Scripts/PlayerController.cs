b9bb8ef6dac1dad83c8b6def954e155535aeb5f3
gianeric08 <gianeric08@gmail.com>
2023-12-24 13:27:56 -0300

    feat: player controler com Move Action

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,53 +5,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start!");
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = GetHorizontal();
-        float vertical = GetVertical();
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + move * 0.01f;
         transform.position = position;
         
         Debug.Log("Posição do X: " + position.x + " Posição do Y: " +position.y);
-    }
-
-    private float GetHorizontal(){
-        float horizontal = 0.0f;
-
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -0.1f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 0.1f;
-        }
-
-        return horizontal;
-    }
-
-    private float GetVertical(){        
-        float vertical = 0.0f;
-
-        if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -0.1f;
-        }
-        else if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 0.1f;
-        }
-
-        return vertical;
     }
 }
