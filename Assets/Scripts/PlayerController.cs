260ee467735f31384b93d859803d5a4b324ede82
gianeric08 <gianeric08@gmail.com>
2023-12-28 18:41:01 -0300

    feat: Add rigidbody e box colider
    Adicionado rigidbody e box colide
    Ajuste no script PlayerController para mover o personagem por rigidbody ao invés de transforme, para que não ocorra o tremor no personagem ao colidir com objeto que tenha box colider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
     public InputAction GamepadAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start.");
         MoveAction.Enable();
-        GamepadAction.Enable();
+        // GamepadAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
 
         // Debug.Log("Initalizing fps.");
         // QualitySettings.vSyncCount = 0;
@@ -23,15 +26,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SetMoveAction(MoveAction);
-        SetMoveAction(GamepadAction);
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
     }
 
-    private void SetMoveAction(InputAction Move){
-        Vector2 move = Move.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
 
-        Debug.Log("Posição do X: " + position.x + " Posição do Y: " +position.y);
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     SetMoveAction(MoveAction);
+    //     SetMoveAction(GamepadAction);
+    // }
+
+    // private void SetMoveAction(InputAction Move)
+    // {
+    //     Vector2 move = MoveAction.ReadValue<Vector2>();
+    //     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+    //     rigidbody2d.MovePosition(position);
+
+    //     // Vector2 move = Move.ReadValue<Vector2>();
+    //     // Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+    //     // transform.position = position;
+
+    //     Debug.Log("Posição do X: " + position.x + " Posição do Y: " +position.y);
+    // }
+
 }
